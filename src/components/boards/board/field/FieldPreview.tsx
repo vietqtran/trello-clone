431dda407de6a81a57509b9e03b9766224e903d5
vietqtran <tranquocviet1303@gmail.com>
2023-10-09 20:20:42 +0700

    Add custom fields

---
+++ src/components/boards/board/field/FieldPreview.tsx
@@ -0,0 +1,12 @@
+import { FieldType } from "@/types"
+import React from "react"
+
+type Props = {
+   field: FieldType
+}
+
+function FieldPreview(props: Props) {
+   return <div className='col-span-1'>dsf</div>
+}
+
+export default FieldPreview
